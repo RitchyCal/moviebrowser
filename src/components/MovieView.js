d9a1a31e5de197733fe1d19bb26fc4b1dd20389e
richy163 <ritchycal@gmail.com>
2024-04-25 17:12:24 -0400

    added more details to movie view

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -5,6 +5,7 @@ import Hero from "./Hero";
 const MovieView = () => {
 	const { id } = useParams();
 	const [movieDetails, setMovieDetails] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 	const fetch = require("node-fetch");
 	useEffect(() => {
 		const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
@@ -19,16 +20,47 @@ const MovieView = () => {
 			.then((res) => res.json())
 			.then((json) => {
 				setMovieDetails(json);
+				setIsLoading(false);
 			})
 			.catch((err) => console.error("error:" + err));
 	}, [fetch, id]);
 
-	console.log(id);
-	return (
-		<>
-			<Hero text={movieDetails.original_title} />
-		</>
-	);
+	const renderMovieDetails = () => {
+		if (isLoading) {
+			return <Hero text="Loading..." />;
+		}
+		if (movieDetails) {
+			//  todo: deal with missing imaheg
+			const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
+			return (
+				<>
+					<Hero text={movieDetails.original_title} />
+					<div className="container my-5">
+						<div className="row">
+							<div className="col-md-3">
+								<img
+									src={posterPath}
+									alt="poster"
+									className="img-fluid shadow rounded"
+								/>
+							</div>
+							<div className="col-md-9">
+								<h2>{movieDetails.original_title}</h2>
+								<p className="lead">{movieDetails.overview}</p>
+								<div className="row">
+									<p>
+										<strong>Revenue: </strong> ${movieDetails.revenue}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</>
+			);
+		}
+	};
+
+	return renderMovieDetails();
 };
 
 export default MovieView;
